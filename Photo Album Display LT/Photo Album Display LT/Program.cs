fcdcfbe6d092e37a11ac440c8b5fbe6317116839
zachammann <51245505+zachammann@users.noreply.github.com>
2019-06-04 19:58:22 -0500

    Console App now loops and checks for empty albums
    Reformated comments, made the application loop until the user gives a quit command, and added feedback for searching an empty album.

--- Photo Album Display LT/Photo Album Display LT/Program.cs
+++ Photo Album Display LT/Photo Album Display LT/Program.cs
@@ -8,55 +8,76 @@ namespace Photo_Album_Display_LT
     {
         static void Main(string[] args)
         {
-            string targetURL = "https://jsonplaceholder.typicode.com/photos?albumId=";//URL setup for grabbing photos from a specific album
-            Console.Write("Please enter an album number to display relevant photo IDs and titles: ");
-            string userInput = Console.ReadLine();
-            int albumSelection = 0;//placeholder to be reassigned
-
-            bool successParse = false;//Exception Handling on User Input - prevent input of non Int values
-            while (successParse.Equals(false))
+            string userInput = "";
+            while (true)
             {
-                successParse = int.TryParse(userInput, out albumSelection);
-                if (successParse)
+                //URL setup for grabbing photos from a specific album
+                string targetURL = "https://jsonplaceholder.typicode.com/photos?albumId=";
+                Console.Write("Please enter an album number to display relevant photo IDs and titles\nor enter \"q\" to quit: ");
+                userInput = Console.ReadLine();
+                if(userInput.Equals("q") || userInput.ToLower().Equals("q"))
                 {
-                    targetURL += albumSelection;// Add album selection to the URL
+                    System.Environment.Exit(1);
+                }
+                int albumSelection = 0;//placeholder to be reassigned
+
+                //Exception Handling on User Input - prevent input of non Int values
+                bool successParse = false;
+                while (successParse.Equals(false))
+                {
+                    successParse = int.TryParse(userInput, out albumSelection);
+                    if (successParse)
+                    {
+                        // Add album selection to the URL
+                        targetURL += albumSelection;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.Write("Your input was not a number. Please try again: ");
+                        userInput = Console.ReadLine();
+                    }
+                }
+                Console.WriteLine("Ready to display IDs and Titles from Album " + albumSelection + ". Please press any key.");
+                Console.ReadKey();
+
+                //Download the relevant data
+                string targetJSON = new WebClient().DownloadString(targetURL);
+                //Check for empty album
+                if (targetJSON.Equals("[]"))
+                {
+                    Console.WriteLine("There are no photos in that album.");
                 }
                 else
                 {
-                    Console.WriteLine();
-                    Console.Write("Your input was not a number. Please try again: ");
-                    userInput = Console.ReadLine();
+                    List<string> idAndTitle = new List<string>();
+
+                    //Split the individual fields apart
+                    String[] individualFields = targetJSON.Split(',');
+                    string result = "";
+                    //Toss out irrelevant information
+                    foreach (string t in individualFields)
+                    {
+                        if (t.Contains("\"id\""))
+                        {
+                            result += t;
+                        }
+                        if (t.Contains("\"title\""))
+                        {
+                            result += t;
+                            result += "\n";
+                        }
+                    }
+                    idAndTitle.Add(result);
+
+                    Console.WriteLine("OUTPUT:");
+                    foreach (string s in idAndTitle)
+                    {
+                        Console.WriteLine(s);
+                    }
                 }
+                Console.ReadKey();
             }
-            Console.WriteLine("Ready to display IDs and Titles from Album " + albumSelection + ". Please press any key.");
-            Console.ReadKey();
-
-            string targetJSON = new WebClient().DownloadString(targetURL); //Download the relevant data
-
-            List<string> idAndTitle = new List<string>();
-            
-            String[] individualFields = targetJSON.Split(','); //Split the individual fields apart
-            string result = "";
-            foreach(string t in individualFields)//Toss out irrelevant information
-            {
-              if(t.Contains("\"id\""))
-              {
-                result += t;
-              }
-              if(t.Contains("\"title\""))
-              {
-                result += t;
-                result += "\n";
-              }
-            }
-            idAndTitle.Add(result);
-            
-            Console.WriteLine("OUTPUT:");
-            foreach(string s in idAndTitle)
-            {
-                Console.WriteLine(s);
-            }
-            Console.ReadKey();
         }
     }
 }
